db33493525534a7c622c0c4ccad5ba4b73abf08e
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2021-10-25 17:47:16 +0400

    added test save in storage

---
+++ test/hermione/catalog.hermione.js
@@ -0,0 +1,14 @@
+describe('catalog test', function() {
+    it('cart saved in storage', async function() {
+        await this.browser.url('/hw/store/catalog');
+        const link = await this.browser.$('.card-link');
+        await link.waitForExist();
+        await link.click();
+        const button = await this.browser.$('.Product button');
+        await button.waitForExist();
+        await button.click();
+        await this.browser.url('/hw/store/cart');
+        const cartTable = await this.browser.$('.Cart-Table');
+        await cartTable.waitForExist();
+    });
+});
