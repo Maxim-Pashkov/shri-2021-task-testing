a3172c8504acfcb02940f1af74e47698fc8ccb1d
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2021-10-25 21:18:02 +0400

    remove snapshot tests for clicked navbar and add one more test

--- test/hermione/markup.hermione.js
+++ test/hermione/markup.hermione.js
@@ -23,15 +23,29 @@ describe('markup test', function() {
         await this.browser.setWindowSize(520, 480);
         const toggler = await this.browser.$('.navbar-toggler');
         await toggler.click();
-        this.browser.execute(() => document.querySelector('.navbar-toggler').blur());
-        await this.browser.assertView('plain', '.navbar', {
-            compositeImage: true,
-            allowViewportOverflow: true,
-        });  
+        const navBar = await this.browser.$('.navbar-nav');
+        await navBar.waitForDisplayed();
+        await this.browser.setWindowSize(width, height);
+    });
+
+    it('navbar mobile opened closed after second click by toggler', async function() {
+        await this.browser.url('/hw/store/');  
+        const {width, height} = await this.browser.getWindowSize();
+        await this.browser.setWindowSize(520, 480);
+        const toggler = await this.browser.$('.navbar-toggler');
+        await toggler.click();
+        await toggler.click();
+        const navBar = await this.browser.$('.navbar-nav');
+        await navBar.waitUntil(async function() {
+            return await navBar.isDisplayed() === false;
+        }, {
+            timeout: 5000,
+            timeoutMsg: 'navbar is displayed after close',
+        });
         await this.browser.setWindowSize(width, height);          
     });
 
-    it('navbar mobile closed after click', async function() {
+    it('navbar mobile closed after click by link', async function() {
         await this.browser.url('/hw/store/');  
         const {width, height} = await this.browser.getWindowSize();   
         await this.browser.setWindowSize(520, 480);
@@ -39,10 +53,13 @@ describe('markup test', function() {
         await toggler.click();
         const link = await this.browser.$('.nav-link');
         await link.click();
-        await this.browser.assertView('plain', '.navbar', {
-            compositeImage: true,
-            allowViewportOverflow: true,
-        });  
+        const navBar = await this.browser.$('.navbar-nav');
+        await navBar.waitUntil(async function() {
+            return await navBar.isDisplayed() === false;
+        }, {
+            timeout: 5000,
+            timeoutMsg: 'navbar is displayed after close',
+        });
         await this.browser.setWindowSize(width, height);          
     });
 });
