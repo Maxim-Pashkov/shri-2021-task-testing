2cbfa1cc43cf9b09615ce179bb0ab6607ab0bbd4
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2021-10-25 23:06:13 +0400

    merge two tests

--- test/hermione/catalog.hermione.js
+++ test/hermione/catalog.hermione.js
@@ -1,25 +1,25 @@
 describe('catalog test', function() {
     it('cart saved in storage', async function() {
         await this.browser.url('/hw/store/catalog');
+
         const link = await this.browser.$('.card-link');
         await link.waitForExist();
         await link.click();
-        const button = await this.browser.$('.Product button');
-        await button.waitForExist();
-        await button.click();
-        await this.browser.url('/hw/store/cart');
-        const cartTable = await this.browser.$('.Cart-Table');
-        await this.browser.execute('localStorage.clear()');
-        await cartTable.waitForExist();
-    });
 
-    it('cart badge is outputted', async function() {
-        await this.browser.url('/hw/store/catalog/0');
         const button = await this.browser.$('.Product button');
         await button.waitForExist();
         await button.click();
+
         const badge = await this.browser.$('.text-success');
-        await this.browser.execute('localStorage.clear()')
-        await badge.waitForExist();        
+        try {
+            await badge.waitForExist(); 
+
+            await this.browser.url('/hw/store/cart');
+
+            const cartTable = await this.browser.$('.Cart-Table');
+            await cartTable.waitForExist();
+        } finally {
+            await this.browser.execute('localStorage.clear()');
+        }      
     });
 });
