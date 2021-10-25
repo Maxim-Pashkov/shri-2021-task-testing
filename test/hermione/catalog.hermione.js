904220b22739b938f8a75b740bc6841ac850292f
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2021-10-25 18:11:30 +0400

    clear localstorage and test output cart badge

--- test/hermione/catalog.hermione.js
+++ test/hermione/catalog.hermione.js
@@ -10,5 +10,16 @@ describe('catalog test', function() {
         await this.browser.url('/hw/store/cart');
         const cartTable = await this.browser.$('.Cart-Table');
         await cartTable.waitForExist();
+        await this.browser.execute('localStorage.clear()')
+    });
+
+    it('cart badge is outputted', async function() {
+        await this.browser.url('/hw/store/catalog/0');
+        const button = await this.browser.$('.Product button');
+        await button.waitForExist();
+        await button.click();
+        const badge = await this.browser.$('.text-success');
+        await badge.waitForExist();
+        await this.browser.execute('localStorage.clear()')
     });
 });
