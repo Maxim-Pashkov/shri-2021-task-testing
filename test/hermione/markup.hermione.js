1cf2bd2706ab5cce8076b4de10e6526a98ff74a8
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2021-10-25 18:46:18 +0400

    test close navbar after click on link

--- test/hermione/markup.hermione.js
+++ test/hermione/markup.hermione.js
@@ -30,4 +30,19 @@ describe('markup test', function() {
         });  
         await this.browser.setWindowSize(width, height);          
     });
+
+    it('navbar mobile closed after click', async function() {
+        await this.browser.url('/hw/store/');  
+        const {width, height} = await this.browser.getWindowSize();   
+        await this.browser.setWindowSize(520, 480);
+        const toggler = await this.browser.$('.navbar-toggler');
+        await toggler.click();
+        const link = await this.browser.$('.nav-link');
+        await link.click();
+        await this.browser.assertView('plain', '.navbar', {
+            compositeImage: true,
+            allowViewportOverflow: true,
+        });  
+        await this.browser.setWindowSize(width, height);          
+    });
 });
