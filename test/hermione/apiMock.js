cbb93d824fd025fa18de65982dcd31e7a5854890
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2021-10-26 21:56:40 +0400

    create api mock helpers

---
+++ test/hermione/apiMock.js
@@ -0,0 +1,29 @@
+const { products, product } = require("../data");
+
+exports.productsMock = async function productsMock(browser) {
+    const dataMock = await browser.mock('**' + '/api/products', {
+        method: 'get'
+    });
+
+    const response = products();
+
+    dataMock.respond(response, {
+        fetchResponse: false,
+    });
+
+    return response;
+};
+
+exports.productMock = async function productMock(id, browser) {
+    const dataMock = await browser.mock('**' + '/api/products/' + id, {
+        method: 'get'
+    });
+
+    const response = product(id);
+
+    dataMock.respond(response, {
+        fetchResponse: false,
+    });
+
+    return response;    
+}
--- test/hermione/catalog.hermione.js
+++ test/hermione/catalog.hermione.js
@@ -1,14 +1,8 @@
-const { products, product } = require("../data");
+const { productsMock, productMock } = require("./apiMock");
 
 describe('catalog test', function() {
     it('check data on page', async function() {
-        const dataMock = await this.browser.mock('**' + '/api/products', {
-            method: 'get'
-        });
-
-        dataMock.respond(products(), {
-            fetchResponse: false,
-        });
+        await productsMock(this.browser);
 
         await this.browser.url('/hw/store/catalog');
         await this.browser.assertView('catalog', '.Catalog', {
@@ -17,13 +11,7 @@ describe('catalog test', function() {
     });
 
     it('check data on page item', async function() {
-        const dataMock = await this.browser.mock('**' + '/api/products/0', {
-            method: 'get'
-        });
-
-        dataMock.respond(product(0), {
-            fetchResponse: false,
-        });
+        await productMock(0, this.browser);
 
         await this.browser.url('/hw/store/catalog/0');
         await this.browser.assertView('product', '.Product', {
@@ -32,34 +20,20 @@ describe('catalog test', function() {
     });
 
     it('check text success on page', async function() {
-        const dataMock = await this.browser.mock('**' + '/api/products/', {
-            method: 'get'
-        });
+        const products = await productsMock(this.browser);
 
-        const productsItems = products();
+        const product = products[0];
 
-        dataMock.respond(productsItems, {
-            fetchResponse: false,
-        });
+        await productMock(product.id, this.browser);
 
-        const productItem = productsItems[0];
-
-        const dataItemMock = await this.browser.mock('**' + '/api/products/' + productItem.id, {
-            method: 'get',
-        });
-
-        dataItemMock.respond(product(productItem.id), {
-            fetchResponse: false,
-        });
-
-        await this.browser.url('/hw/store/catalog/' + productItem.id);
+        await this.browser.url('/hw/store/catalog/' + product.id);
 
         try {
             await addToCart(this.browser);    
             
             await this.browser.url('/hw/store/catalog/');
 
-            await this.browser.assertView('item in cart', `.ProductItem[data-testid="${productItem.id}"]`, {
+            await this.browser.assertView('item in cart', `.ProductItem[data-testid="${product.id}"]`, {
                 compositeImage: true,
             });
         } finally {
@@ -68,13 +42,7 @@ describe('catalog test', function() {
     });
 
     it('check text success on page item', async function() {
-        const dataMock = await this.browser.mock('**' + '/api/products/0', {
-            method: 'get'
-        });
-
-        dataMock.respond(product(0), {
-            fetchResponse: false,
-        });
+        await productMock(0, this.browser);
 
         await this.browser.url('/hw/store/catalog/0');
 
@@ -103,6 +71,6 @@ describe('catalog test', function() {
         await browser.execute((selector) => {
             document.querySelector(selector).blur();
         }, '.ProductDetails-AddToCart');
-        await new Promise(resolve => setTimeout(resolve, 100));
+        await new Promise(resolve => setTimeout(resolve, 200));
     }
 });
