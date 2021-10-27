88e8180c75ed4b8a84db55402f4d252fcb08f7dd
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2021-10-27 16:31:04 +0400

    Revert "check text 'item in cart' on catalog page"
    This reverts commit 1dc9d57a53083c2f2cddff6057a5781a2f64f85a.

--- test/hermione/catalog.hermione.js
+++ test/hermione/catalog.hermione.js
@@ -31,42 +31,6 @@ describe('catalog test', function() {
         });
     });
 
-    it('check text success on page', async function() {
-        const dataMock = await this.browser.mock('**' + '/api/products/', {
-            method: 'get'
-        });
-
-        const productsItems = products();
-
-        dataMock.respond(productsItems, {
-            fetchResponse: false,
-        });
-
-        const productItem = productsItems[0];
-
-        const dataItemMock = await this.browser.mock('**' + '/api/products/' + productItem.id, {
-            method: 'get',
-        });
-
-        dataItemMock.respond(product(productItem.id), {
-            fetchResponse: false,
-        });
-
-        await this.browser.url('/hw/store/catalog/' + productItem.id);
-
-        try {
-            await addToCart(this.browser);    
-            
-            await this.browser.url('/hw/store/catalog/');
-
-            await this.browser.assertView('item in cart', `.ProductItem[data-testid="${productItem.id}"]`, {
-                compositeImage: true,
-            });
-        } finally {
-            await this.browser.execute('localStorage.clear()');
-        }        
-    });
-
     it('check text success on page item', async function() {
         const dataMock = await this.browser.mock('**' + '/api/products/0', {
             method: 'get'
@@ -78,14 +42,23 @@ describe('catalog test', function() {
 
         await this.browser.url('/hw/store/catalog/0');
 
+        const addToCart = async () => {
+            const button = await this.browser.$('.ProductDetails-AddToCart');
+            await button.waitForClickable();
+            await button.click();
+            await this.browser.execute((selector) => {
+                document.querySelector(selector).blur();
+            }, '.ProductDetails-AddToCart');
+        };
+
         try {
-            await addToCart(this.browser);       
+            await addToCart();       
 
             await this.browser.assertView('one item in cart', '.Product', {
                 compositeImage: true,
             });
 
-            await addToCart(this.browser);
+            await addToCart();
 
             await this.browser.assertView('two items in cart', '.Product', {
                 compositeImage: true,
@@ -95,14 +68,4 @@ describe('catalog test', function() {
         }
         
     });
-
-    async function addToCart(browser) {
-        const button = await browser.$('.ProductDetails-AddToCart');
-        await button.waitForClickable();
-        await button.click();
-        await browser.execute((selector) => {
-            document.querySelector(selector).blur();
-        }, '.ProductDetails-AddToCart');
-        await new Promise(resolve => setTimeout(resolve, 100));
-    }
 });
