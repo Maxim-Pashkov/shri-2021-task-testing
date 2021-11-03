548776638684b7e992aa749ddc6c2e748abe619f
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2021-11-03 23:07:43 +0400

    added proofs active links

--- test/unit/Application.test.tsx
+++ test/unit/Application.test.tsx
@@ -3,6 +3,7 @@ import { it, expect, describe } from '@jest/globals';
 import { render } from '@testing-library/react';
 import { getApplication } from './getApplication';
 import { Application } from '../../src/client/Application';
+import userEvent from '@testing-library/user-event';
 
 describe('Application test', () => {
     it('navbar is exists', () => {
@@ -11,6 +12,25 @@ describe('Application test', () => {
         const { container } = render(application);
 
         expect(container.querySelector('.navbar')).toMatchSnapshot();
+    });
+
+    it('navbar active links', () => {
+        const { application } = getApplication(() => <Application />, {});
+
+        const { container } = render(application);
+
+        const nav = container.querySelector('.navbar-nav');
+
+        const links = ['/catalog', '/delivery', '/delivery', '/cart']
+            .map(href => nav?.querySelector(`a[href="${href}"]`))
+            .filter(Boolean);
+
+        links.forEach(link => {
+            userEvent.click(link);
+            links.forEach(otherLink => {
+                expect(otherLink.classList.contains('active')).toBe(otherLink === link);
+            });
+        });
     });
 
     it('Cart link output count items in cart', () => {
